507d60ff570c73686f82baec0036f3e935ac82cf
Qesem1999 <46812936+Qesem1999@users.noreply.github.com>
2019-04-07 22:10:14 +0400

    Add files via upload

--- tapsiriq3/js/script.js
+++ tapsiriq3/js/script.js
@@ -1,27 +1,93 @@
-"use strict"
+"use strict";
+
+let manu = document.forms["userForm"]["Manufacturer"];
+    let model = document.forms["userForm"]["Model"];
+    let year = document.forms["userForm"]["year"];
+let ullist = document.querySelector("#car-list")
+let onlyLatter = /^[a-zA-Z]*$/;
+let onlyNumber = /([0-9]{4})/;
+let carArray = [];
 
 
-let next = document.querySelector(".arrowright");
-let prev = document.querySelector(".arrowleft");
 
-next.addEventListener("click", nextSlide);
-prev.addEventListener("click", prevSlide);
 
-function nextSlide(){
-    let activeImage = document.querySelector(".active-next-item");
-    activeImage.classList.remove("active-next-item");
-    if(activeImage.nextElementSibling != null){
-        activeImage.nextElementSibling.classList.add("active-next-item");
-    }else{
-        activeImage.parentElement.firstElementChild.classList.add("active-next-item")
-    }
+
+function Cars(nomresi, istehsalci, marka, buraxlisili){
+    this.id = nomresi;
+    this.manu = istehsalci;
+    this.model = marka;
+    this.year = buraxlisili;
 }
-function prevSlide(){
-    let activeImage = document.querySelector(".active-next-item");
-    activeImage.classList.remove("active-next-item");
-    if(activeImage.previousElementSibling != null){
-        activeImage.previousElementSibling.classList.add("active-next-item");
-    }else{
-        activeImage.parentElement.lastElementChild.classList.add("active-next-item")
+
+
+let idCounter = (function () {
+    var counter = 0;
+    return function () {
+        return counter += 1;
     }
+})();
+
+function validateForm() {
+    
+
+    if(manu.value == "" || model.value == "" || year.value == ""){
+        alert("Any of inputs is empty");
+        return false;
+    }
+    if(!manu.value.match(onlyLatter)){
+        alert("Manufacturer input is false");
+        return false;
+        
+    }
+    if(!year.value.match(onlyNumber)){
+        alert("Release year is false");
+        return false;
+    }
+    if(new Date() < new Date(year.value)){
+        alert("Release year is wrong");
+        return false;
+    }
+
+
+
+    let newCar = new Cars(idCounter(), manu.value, model.value, year.value);
+
+    carArray.push(newCar);
+   
+    displayList();
+   resetInputs();
+
+   return false;
+   
+   
 }
+function deleteId(event){
+    let elementIndex = event.parentElement;
+    let id = +elementIndex.getAttribute("data-id");
+    carArray.splice(id,1);
+    displayList()
+}
+
+
+
+
+function displayList(){
+    let list = "";
+    for(let i = 0; i < carArray.length; i++){
+       list += "<li class='list-group-item d-flex justify-content-between' data-id='"+i+"' onclick='fillForm()'><p class='m-0'>" + carArray[i].id + ". " + carArray[i].manu + " " + carArray[i].model + " - "
+       + carArray[i].year + '</p><a href="#" onclick="deleteId(this)"><i class="far fa-trash-alt text-danger"></i></a></li>';
+    }
+    
+    
+    ullist.innerHTML = list;
+}
+function resetInputs() {
+    manu.value = "";
+    model.value = "";
+    year.value = "";
+    
+}
+
+
+
+
